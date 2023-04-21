eab5cd510543fd5531d920774fa1defdaf6b9caa
IThundxr <contact@ithundxr.dev>
2023-04-21 11:45:39 -0400

    Update server.ts

--- src/pages/.well-known/matrix/server.ts
+++ src/pages/.well-known/matrix/server.ts
@@ -1,10 +1,10 @@
 import type { APIRoute } from "astro";
 
 export const get: APIRoute = async () => {
-  return {
+  return new Response(
     JSON.stringify({
       "m.server": "matrix.ithundxr.dev:443"
     }),
-      { status: 200 }
-  }
+    { status: 200 }
+  );
 };
