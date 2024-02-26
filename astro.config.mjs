2dd10db9be0fd4773757f8b2ed2d9b7f23586582
IThundxr <me@ithundxr.dev>
2024-02-26 17:04:20 -0500

    cleanup of website

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,9 @@ import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import robotsTxt from "astro-robots-txt";
+import icon from "astro-icon";
 
 export default defineConfig({
 	site: "https://ithundxr.dev",
-	integrations: [react(), tailwind(), sitemap(), robotsTxt()],
+	integrations: [react(), tailwind(), sitemap(), robotsTxt(), icon()],
 });
